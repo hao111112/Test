58c16f28dfba788d678cdd3827cb5d403197bbee
Hystrix <2669731677@qq.com>
2024-03-07 14:32:09 +0800

    0307

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app=express()
+// 将 public 文件夹下的内容作为静态资源进行托管
+app.use(express.static('public'));
+const port = 8083;
+app.listen(port,()=>{
+    console.log(`Server running at http://localhost:${port}/`)
+});
